2b50a12ad6310c3d0b77fef28f2c67b7608d4d0f
dahyun <halover111@gmail.com>
2024-10-14 06:55:40 +0000

    회전 구현

--- src/rbtree.c
+++ src/rbtree.c
@@ -26,14 +26,47 @@ void delete_rbtree_sub(rbtree *t, node_t *p) {
   }
 }
 
-
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // 구현하는 ADT가 multiset이므로 이미 같은 key의 값이 존재해도 하나 더 추가 합니다.
+
 
   return t->root;
 }
 
+void left_rotate(rbtree *t, node_t *curTop) {
+  node_t *targetTop = curTop->right;
+  curTop->right = targetTop->left;                                  //targetTop의 왼쪽 서브트리를 curTop의 오른쪽 서브트리로 회전
+  if (targetTop->left != t->nil) targetTop->left->parent = curTop;  //targetTop이 왼쪽 자식노드를 가지고 있다면 curTop의 오른쪽 자식으로 바꿔주기
+  targetTop->parent = curTop->parent;                               //targetTop의 부모노드 업데이트
+
+  // curTop이 root인 경우
+  if (curTop->parent == NULL) t->root = targetTop;
+  // curTop이 왼쪽 자식인 경우
+  else if (curTop == curTop->parent->left) curTop->parent->left = targetTop;
+  // curTop이 오른쪽 자식인 경우
+  else curTop->parent->right = targetTop;
+
+  // targetTop의 왼쪽 자식과 curTop의 부모 노드 업데이트
+  targetTop->left = curTop;
+  curTop->parent = targetTop;
+}
+
+void right_rotate(rbtree *t, node_t *curTop) {
+  node_t *targetTop = curTop->right;
+  curTop->left = targetTop->right;                                      //targetTop의 오른쪽 서브트리를 curTop의 왼쪽 서브트리로 회전
+  if (targetTop->right != t->nil) targetTop->right->parent = curTop;    //targetTop이 오른쪽 자식노드를 가지고 있다면 curTop의 왼쪽 자식으로 바꿔주기
+  targetTop->parent = curTop->parent;                                   //targetTop의 부모노드 업데이트
+  
+  if(curTop->parent == t->nil) t->root = targetTop;
+  else if (curTop == curTop->parent->right) curTop->parent->right = targetTop;
+  else curTop->parent->left = targetTop;
+
+  targetTop->right = curTop;
+  curTop->parent = targetTop;
+}
+
 void *rbtree_insert_fixup(rbtree *t, const key_t key) {
+  //불균형 복구
 
 }
 
