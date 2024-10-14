57a6c3ecfa2bffe123ee837b8c78fabafd22962e
dahyun <halover111@gmail.com>
2024-10-14 04:44:23 +0000

    메모리 해제 구현 완료

--- src/rbtree.c
+++ src/rbtree.c
@@ -11,36 +11,54 @@ rbtree *new_rbtree(void) {
 }
 
 void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
-  free(t);
+  // 해당 tree가 사용했던 메모리를 전부 반환해야 합니다. (valgrind로 나타나지 않아야 함)
+  delete_rbtree_sub(t, t->root);    //노드들 메모리 전부 해제
+  free(t->nil);                     //nil 메모리 해제 (root는 노드 메모리 해제할 때 기수행)
+  free(t);                          //구조체 메모리 해제
 }
 
+void delete_rbtree_sub(rbtree *t, node_t *p) {
+  if (p != t->nil) {
+    //재귀적으로 메모리 해제 수행
+    delete_rbtree_sub(t, p->left);    
+    delete_rbtree_sub(t, p->right);
+    free(p);
+  }
+}
+
+
 node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
+  // 구현하는 ADT가 multiset이므로 이미 같은 key의 값이 존재해도 하나 더 추가 합니다.
+
   return t->root;
 }
 
+void *rbtree_insert_fixup(rbtree *t, const key_t key) {
+
+}
+
 node_t *rbtree_find(const rbtree *t, const key_t key) {
-  // TODO: implement find
+  // RB tree내에 해당 key가 있는지 탐색하여 있으면 해당 node pointer 반환, 없으면 NULL 반환
+
   return t->root;
 }
 
 node_t *rbtree_min(const rbtree *t) {
-  // TODO: implement find
+  // 노드 중 가장 작은 키 값 반환
   return t->root;
 }
 
 node_t *rbtree_max(const rbtree *t) {
-  // TODO: implement find
+  // 노드 중 가장 큰 키 값 반환
   return t->root;
 }
 
 int rbtree_erase(rbtree *t, node_t *p) {
-  // TODO: implement erase
+  // 노드 삭제
   return 0;
 }
 
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
-  // TODO: implement to_array
+  // RB tree의 내용을 key 순서대로 주어진 array로 변환
   return 0;
 }
